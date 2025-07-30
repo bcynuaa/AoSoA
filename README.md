a131e8d1896f07f320b939745654786f2c862e70
bcynuaa <bcynuaa@163.com>
2025-07-30 18:03:28 +0800

    finish

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AoSoA
 
 A package to evaluate AoS or SoA by KernelAbstractions.jl
+
+```julia
+julia --project=. src/AoSoA.jl --config config.json
+```
+
+See `configs/config.json` as an example configuration file.
--- src/AoSoA.jl
+++ src/AoSoA.jl
@@ -6,11 +6,18 @@
   @ description:
  =#
 
+# * ==================== packages ==================== * #
+
 using JSON
 using DataFrames
 using CSV
 using KernelAbstractions
 using ArgParse
+using ThreadPinning
+using OrderedCollections
+pinthreads(:cores)
+
+# * ==================== settings ==================== * #
 
 __ARGPARSE = ArgParseSettings()
 @add_arg_table __ARGPARSE begin
@@ -20,16 +27,47 @@ __ARGPARSE = ArgParseSettings()
       default = "configs/config.json"
 end
 
-const ARGPARSE = parse_args(__ARGPARSE)
-println(ARGPARSE)
+if ispath("data") == false
+    mkpath("data")
+end
 
+const ARGPARSE = parse_args(__ARGPARSE)
+const CONFIG = JSON.parsefile(ARGPARSE["config"])
 const IT = Int32
 const FT = Float32
+const backend = CONFIG["backend"]
+const device = CONFIG["device"]
+const n_threads = CONFIG["n_threads"]
+const n_neighbourhood = CONFIG["n_neighbourhood"]
+const n_loops = CONFIG["n_loops"]
+const n_list = CONFIG["n_list"] .|> IT
+const data_path = "data/$(device).csv"
+if backend == "cpu"
+    const CT = Array
+    const Backend = KernelAbstractions.CPU()
+elseif backend == "cuda"
+    using CUDA
+    const CT = CUDA.CuArray
+    const Backend = CUDA.CUDABackend()
+elseif backend == "rocm"
+    using AMDGPU
+    const CT = AMDGPU.ROCArray
+    const Backend = AMDGPU.ROCBackend()
+elseif backend == "oneapi"
+    using oneAPI
+    const CT = oneAPI.oneArray
+    const Backend = oneAPI.oneAPIBackend()
+elseif backend == "metal"
+    using Metal
+    const CT = Metal.MtlArray
+    const Backend = Metal.MetalBackend()
+end
+
+# * ==================== kernel function for RowStruct or ColumnStruct ==================== * #
 
 @kernel function addByRowStruct!(neighbourhood, index, data)
     i::IT = @index(Global)
     n::IT = @inbounds neighbourhood[i]
-    count::IT = 0
     # * selfaction
     @inbounds data[i, 1] += data[i, 2]
     @inbounds data[i, 2] *= data[i, 3]
@@ -41,8 +79,9 @@ const FT = Float32
     @inbounds data[i, 98] *= data[i, 99]
     @inbounds data[i, 99] += data[i, 1]
     # * interaction
+    count::IT = 0
     while count < n
-        j = @inbounds index[i, count]
+        j = @inbounds index[i, count + 1]
         @inbounds data[i, 1] += data[j, 2]
         @inbounds data[i, 2] *= data[j, 3]
         @inbounds data[i, 3] += data[j, 4]
@@ -60,7 +99,6 @@ end
 @kernel function addByColumnStruct!(neighbourhood, index, data)
     i::IT = @index(Global)
     n::IT = @inbounds neighbourhood[i]
-    count::IT = 0
     # * selfaction
     @inbounds data[1, i] += data[2, i]
     @inbounds data[2, i] *= data[3, i]
@@ -72,8 +110,9 @@ end
     @inbounds data[98, i] *= data[99, i]
     @inbounds data[99, i] += data[1, i]
     # * interaction
+    count::IT = 0
     while count < n
-        j = @inbounds index[i, count]
+        j = @inbounds index[count + 1, i]
         @inbounds data[1, i] += data[2, j]
         @inbounds data[2, i] *= data[3, j]
         @inbounds data[3, i] += data[4, j]
@@ -87,3 +126,86 @@ end
         count += 1
     end
 end
+
+# * ==================== array prepare ==================== * #
+
+@inbounds function prepareArray(n, transpose = false)
+    interval = ceil(n / n_neighbourhood) |> IT
+    if transpose == false
+        index = zeros(IT, n, n_neighbourhood)
+        data = randn(FT, n, 100)
+        Threads.@threads for i in 1:n
+            for j in 1:n_neighbourhood
+                id = i + (j - 1) * interval
+                id = mod1(id, n)
+                @inbounds index[i, j] = id
+            end
+        end
+    else
+        index = zeros(IT, n_neighbourhood, n)
+        data = randn(FT, 100, n)
+        Threads.@threads for i in 1:n
+            for j in 1:n_neighbourhood
+                id = i + (j - 1) * interval
+                id = mod1(id, n)
+                @inbounds index[j, i] = id
+            end
+        end
+    end
+    return index, data
+end
+
+@inbounds function parseTimedReport(report)::OrderedDict{String, Float64}
+    timed_report = OrderedDict{String, Float64}()
+    timed_report["time"] = report.time
+    timed_report["gctime"] = report.gctime
+    timed_report["compile_time"] = report.compile_time
+    timed_report["recompile_time"] = report.recompile_time
+    return timed_report
+end
+
+@inbounds function singleRun(n, transpose)::Tuple
+    index, data = prepareArray(n, transpose)
+    index = CT(index)
+    data = CT(data)
+    neighbourhood = zeros(IT, n)
+    fill!(neighbourhood, n_neighbourhood)
+    neighbourhood = CT(neighbourhood)
+    # * cause compilation
+    if transpose == false
+        kernel! = addByRowStruct!(Backend, n_threads, (Int64(n),))
+    else
+        kernel! = addByColumnStruct!(Backend, n_threads, (Int64(n),))
+    end
+    kernel!(neighbourhood, index, data, ndrange = (n,))
+    # * run kernel
+    report = @timed begin
+        for _ in 1:n_loops
+            kernel!(neighbourhood, index, data, ndrange = (n,))
+        end
+    end
+    index = nothing
+    data = nothing
+    neighbourhood = nothing
+    GC.gc()
+    timed_report = parseTimedReport(report)
+    major = transpose == false ? "row" : "column"
+    return Tuple(vcat(
+        [device, backend, major, n],
+        Float64.(collect(values(timed_report))),
+    ))
+end
+
+@inline function main()
+    df = DataFrame(device=String[], backend=String[], major=String[], n = Int[], time=Float64[], gctime=Float64[], compile_time=Float64[], recompile_time=Float64[])
+    for n in n_list
+        for transpose in (false, true)
+            println("Running for n = $n, transpose = $transpose")
+            result = singleRun(n, transpose)
+            push!(df, result)
+            CSV.write(data_path, df, header = true, delim=",")
+        end
+    end
+end
+
+main()
